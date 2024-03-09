7761fbd3fae9ada6b16cf5d9684a8b8ba67ea400
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-03-09 20:28:32 +0900

    feature: レスポンシブ対応

--- frontend/src/components/Hint.tsx
+++ frontend/src/components/Hint.tsx
@@ -3,7 +3,7 @@ type HintProps = {
 } 
 
 export const Hint: React.FC<HintProps> = ({hints}) => {
-  const hintsItems: Array<JSX.Element> = hints.map(hints => <span className="p-6 badge badge-lg badge-primary badge-outline ml-2 text-xl" key={hints}>{hints}</span>)
+  const hintsItems: Array<JSX.Element> = hints.map(hints => <span className="md:p-6 badge badge-lg badge-primary badge-outline ml-4 md:text-xl" key={hints}>{hints}</span>)
   return (
     <div>
       {hintsItems}
