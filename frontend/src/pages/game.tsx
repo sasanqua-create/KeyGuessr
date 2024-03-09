df7d724ae12614061eaa52d26932c336fad5f453
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-03-09 21:25:16 +0900

    feature: プレイ中かリザルトかのページを切り替えるコンポーネントの追加

--- frontend/src/pages/game.tsx
+++ frontend/src/pages/game.tsx
@@ -1,23 +1,11 @@
-import { Hint } from '../components/game/Hint'
-import { Answer } from '../components/game/Answer'
-import { Question } from '../components/game/Question'
+import { Play } from './play'
+import { Result } from './result'
 
 export const Game: React.FC = () => {
-  return (
-    <div className="h-full flex-1">
-      <div className="h-1/6">
-        <Hint hints={['ヒント1', 'ヒント2','ヒント3','ヒント4','ヒント5']} />
-      </div>
-      <div className="h-2/3 flex items-center justify-center">
-        <Question />
-      </div>
-      <div className="h-1/6 flex items-center justify-center">
-        <div className="w-1/4"></div>
-        <div className="w-1/2">
-          <Answer />
-        </div>
-        <div className="w-1/4"></div>
-      </div>
-    </div>
-  )
+  const constTrue = true
+  if (constTrue) {
+    return <Play />
+  } else {
+    return <Result />
+  }
 }
---
+++ frontend/src/pages/play.tsx
@@ -0,0 +1,23 @@
+import { Hint } from '../components/game/Hint'
+import { Answer } from '../components/game/Answer'
+import { Question } from '../components/game/Question'
+
+export const Play: React.FC = () => {
+  return (
+    <div className="h-full flex-1">
+      <div className="h-1/6">
+        <Hint hints={['ヒント1', 'ヒント2', 'ヒント3', 'ヒント4', 'ヒント5']} />
+      </div>
+      <div className="h-2/3 flex items-center justify-center">
+        <Question />
+      </div>
+      <div className="h-1/6 flex items-center justify-center">
+        <div className="w-1/4"></div>
+        <div className="w-1/2">
+          <Answer />
+        </div>
+        <div className="w-1/4"></div>
+      </div>
+    </div>
+  )
+}
