4a2c8b62120c549df07c6cc0987d1acc9a5d92a5
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-03-09 21:09:31 +0900

    fix: ボタンをprimary色に変更

--- frontend/src/pages/game.tsx
+++ frontend/src/pages/game.tsx
@@ -5,7 +5,7 @@ export const Game: React.FC = () => {
   return (
     <div className="h-full flex-1">
       <div className="h-1/6">
-        <Hint hints={['ヒント1', 'ヒント2']} />
+        <Hint hints={['ヒント1', 'ヒント2','ヒント3','ヒント4','ヒント5']} />
       </div>
       <div className="h-2/3"></div>
       <div className="h-1/6 flex items-center justify-center">
