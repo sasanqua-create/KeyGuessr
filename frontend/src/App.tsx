182255a876aa348e2a99247e6077b7ffd3f30d8e
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-03-09 09:02:20 +0000

    feature: header-compとrouterの実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Game } from './pages/game.js'
+import { Home } from './pages/home.js'
+import { Header } from './components/Header.js'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Header />
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/game/" element={<Game />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   )
 }
