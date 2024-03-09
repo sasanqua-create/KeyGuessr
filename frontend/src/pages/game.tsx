60d75127b7e979e8d9d65f8d335e2eac077d195a
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-03-09 21:21:35 +0900

    feature: resultページの作成

--- frontend/src/pages/game.tsx
+++ frontend/src/pages/game.tsx
@@ -1,5 +1,6 @@
 import { Hint } from '../components/game/Hint'
 import { Answer } from '../components/game/Answer'
+import { Question } from '../components/game/Question'
 
 export const Game: React.FC = () => {
   return (
@@ -7,7 +8,9 @@ export const Game: React.FC = () => {
       <div className="h-1/6">
         <Hint hints={['ヒント1', 'ヒント2','ヒント3','ヒント4','ヒント5']} />
       </div>
-      <div className="h-2/3"></div>
+      <div className="h-2/3 flex items-center justify-center">
+        <Question />
+      </div>
       <div className="h-1/6 flex items-center justify-center">
         <div className="w-1/4"></div>
         <div className="w-1/2">
---
+++ frontend/src/pages/result.tsx
@@ -0,0 +1,12 @@
+export const Result: React.FC = () => {
+  return (
+    <div className="h-full flex-1">
+      <div className="h-2/3 flex items-center justify-center"></div>
+      <div className="h-1/6 flex items-center justify-center">
+        <div className="w-1/4"></div>
+        <div className="w-1/2"></div>
+        <div className="w-1/4"></div>
+      </div>
+    </div>
+  )
+}
