2ad1e596ed5e2c2b6cd130c309d748c2c325c1e0
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-03-09 04:28:21 +0000

    feature: ファイルの作成

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+      <ul>
+        <li>リスト1</li>
+        <li>リスト2</li>
+        <li>リスト3</li>
+        <li>リスト4</li>
+      </ul>
+    );
+  };
+  
+  export { Header };
